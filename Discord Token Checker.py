08ad6f2ce055e24ae4558f2b7f5e2d84b4f09607
Happy enderman <86417745+happyenderman10@users.noreply.github.com>
2021-10-22 20:14:24 +0100

    Update Discord Token Checker.py

--- Discord Token Checker.py
+++ Discord Token Checker.py
@@ -4,11 +4,11 @@ token = input("Enter the account token : ")
 checker = {
 	"Authorization": token
 	}
-e = requests.post('https://discord.com/api/v9/auth/login', 
+res = requests.post('https://discord.com/api/v9/auth/login', 
 headers=checker)
 
-if e.status_code == 200:
+if res.status_code == 200:
     print("The token is Valid!")
-if e.status_code == 400:
+if res.status_code == 400:
     print("The token is Invalid")
       
